778830e4ca38b610daa2711b9ee7cd637ce7d56c
You Matsumoto <youmts22@gmail.com>
2019-11-24 08:14:14 +0900

    access_log

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -26,19 +26,23 @@ class SlackController < ApplicationController
     query = "from:@#{user_id} on:2019-11-24"
     debug_messages << query
     search_result = client.search_all(query: query)
-    debug_messages << search_result.messages.matches.map { |x| [x.user, Time.at(x.ts.to_f).in_time_zone] }
+    debug_messages << search_result.messages.matches.map { |x| [x.user, to_tz(x.ts)] }
 
     query = "on:2019-11-24"
     debug_messages << query
     search_result = client.search_messages(query: query)
-    debug_messages << search_result.messages.matches.map { |x| [x.user, Time.at(x.ts.to_f).in_time_zone] }
+    debug_messages << search_result.messages.matches.map { |x| [x.user, to_tz(x.ts)] }
 
     debug_messages << "team.accessLogs"
     access_logs = client.team_accessLogs
-    debug_messages << access_logs.logins.map { |x| [x.user_id, x.username, x.date_first, x.date_last, x.count] }
+    debug_messages << access_logs.logins.map { |x| [x.user_id, x.username, to_tz(x.date_first), to_tz(x.date_last), x.count] }
 
     session[:debug] = debug_messages.map(&:pretty_inspect).join("\n")
 
     redirect_to '/'
+  end
+
+  def to_tz(ts)
+    Time.at(ts.to_f).in_time_zone
   end
 end
